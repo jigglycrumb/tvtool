6af92f037954b2e6aedc263204180ce0d335a8f7
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2017-04-02 01:37:52 +0200

    Webpack (#1)
    * Webpack setup
    
    * Duplicated sources to new src folder
    * Updated package.json to install webpack instead of grunt
    * Added simple webpack config
    * Extracted AppState into it's own module
    
    * App setup
    
    * Enabled JSX transpiling
    * Added Bootstrap CSS
    * Refactored Header and Footer components
    
    * Further refactoring of React components
    
    * Split Search into Container and Presentational components, moved it to redux
    
    * Moves TvShow, language and season selection to redux
    
    * Text changes
    
    * Completed migration to redux
    
    * Cleanup of old sources

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import bootstrap from 'bootstrap/dist/css/bootstrap';
+import theMovieDb from 'themoviedb-javascript-library';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ZeroClipboard from 'zeroclipboard';
+import ZeroClipboardSWF from 'zeroclipboard/dist/ZeroClipboard.swf';
+import styles from './styles/app';
+import App from './views/App.jsx';
+import store from './state/store';
+import { Provider, connect } from 'react-redux';
+
+store.subscribe(() => {
+  // console.log('Store changed', store.getState());
+  localStorage.setItem('TvTool', JSON.stringify(store.getState()));
+});
+
+theMovieDb.common.api_key = "0c00851b985d8ed85c29492b352c717e";
+ZeroClipboard.config({ swfPath: ZeroClipboardSWF });
+
+window.onload = function() {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('app')
+  );
+};
---
+++ src/containers/TvShowContainer.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import theMovieDb from 'themoviedb-javascript-library';
+import TvShow from '../views/TvShow';
+import actions from '../state/actions';
+const { loadShowInfoSuccess, setZerofill, setSpaceReplacement, selectSeason } = actions;
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.language,
+    show: state.show,
+    info: state.showdata.info,
+    space: state.space,
+    zerofill: state.zerofill,
+    season: state.season
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadShowInfoSuccess: (info) => dispatch(loadShowInfoSuccess(info)),
+    setZerofill: (zerofill) => dispatch(setZerofill(zerofill)),
+    setSpaceReplacement: (space) => dispatch(setSpaceReplacement(space)),
+    selectSeason: (season) => dispatch(selectSeason(season))
+  };
+};
+
+const TvShowContainer = React.createClass({
+  render: function() {
+    return (
+      <TvShow
+        language={this.props.language}
+        show={this.props.show}
+        info={this.props.info}
+        space={this.props.space}
+        season={this.props.season}
+        zerofill={this.props.zerofill}
+        setZerofill={this.props.setZerofill}
+        setSpaceReplacement={this.props.setSpaceReplacement}
+        selectSeason={this.props.selectSeason} />
+    );
+  },
+
+  componentDidMount: function() {
+    this.loadShow(this.props.show, this.props.language);
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    if(this.props.show !== nextProps.show || this.props.language !== nextProps.language) {
+      this.loadShow(nextProps.show, nextProps.language);
+    }
+  },
+
+  loadShow: function(id, language) {
+    if(id !== null) {
+      theMovieDb.tv.getById({"id": id, "language": language}, this.loadShowSuccess, this.loadShowError);
+    }
+  },
+
+  loadShowSuccess: function(json) {
+    json = JSON.parse(json);
+    this.props.loadShowInfoSuccess(json);
+  },
+
+  loadShowError: function(json) {
+    json = JSON.parse(json);
+    console.error('TvShow.loadShowError', json);
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TvShowContainer);
