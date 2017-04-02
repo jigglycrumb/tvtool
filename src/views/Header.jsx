d165852da4f8a459187d5638a7fe898a62655bac
jigglycrumb <1476865+jigglycrumb@users.noreply.github.com>
2017-04-02 02:05:51 +0200

    Refactors Header and Footer to stateless functional components

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-export default class Header extends React.Component {
-  render() {
-    return (
-      <header className="page-header">
-        <div className="backdrop"></div>
-        <div className="backdrop-overlay"></div>
-        <h1 className="row">
-          <span className="col-xs-10 col-xs-offset-1">
-            ShowTool<br />
-            <small>Rename tv show episodes with ease.</small>
-          </span>
-        </h1>
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className="page-header">
+      <div className="backdrop"></div>
+      <div className="backdrop-overlay"></div>
+      <h1 className="row">
+        <span className="col-xs-10 col-xs-offset-1">
+          TvTool<br />
+          <small>Rename tv show episodes with ease.</small>
+        </span>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
