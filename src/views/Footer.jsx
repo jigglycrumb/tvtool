9525613ab69e894c2295201aa2ed96710b2b7bce
hpcodecraft <thesquidpeople@googlemail.com>
2017-04-02 02:05:51 +0200

    Refactors Header and Footer to stateless functional components

--- src/views/Footer.jsx
+++ src/views/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export default class Footer extends React.Component {
-  render() {
-    var year = new Date().getFullYear();
+const Footer = () => {
+  var year = new Date().getFullYear();
 
-    return (
-      <footer className="row text-center text-muted">
-        <p className="col-xs-12">This product uses the <a href="http://www.themoviedb.org/" target="_blank">TMDb</a> API but is not endorsed or certified by <a href="http://www.themoviedb.org/" target="_blank">TMDb</a>.</p>
-        <p className="col-xs-12">&copy; <a href="http://hpcodecraft.me">hpcodecraft</a> 2014-{year}</p>
-      </footer>
-    );
-  }
-}
+  return (
+    <footer className="row text-center text-muted">
+      <p className="col-xs-12">This product uses the <a href="http://www.themoviedb.org/" target="_blank">TMDb</a> API but is not endorsed or certified by <a href="http://www.themoviedb.org/" target="_blank">TMDb</a>.</p>
+      <p className="col-xs-12">&copy; <a href="http://hpcodecraft.me">hpcodecraft</a> 2014-{year}</p>
+    </footer>
+  );
+};
+
+export default Footer;
