be89db4a704462fca150ab44cce395436ce3cff0
hpcodecraft <thesquidpeople@gmail.com>
2022-02-09 10:18:44 +0100

    fix: moviedb https

--- src/app.js
+++ src/app.js
@@ -13,8 +13,10 @@ store.subscribe(() => {
 });
 
 theMovieDb.common.api_key = "0c00851b985d8ed85c29492b352c717e";
+theMovieDb.common.base_uri = "https://api.themoviedb.org/3/";
+theMovieDb.common.images_uri = "https://image.tmdb.org/t/p/";
 
-window.onload = function() {
+window.onload = function () {
   ReactDOM.render(
     <Provider store={store}>
       <App />
